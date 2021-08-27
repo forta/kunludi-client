0f23eec953d02a64ad61c07501f205c940860935
Francisco <francisco.orta@gmail.com>
2021-08-27 12:04:43 +0100

    Update index.js

--- config/index.js
+++ config/index.js
@@ -13,8 +13,11 @@ module.exports = {
     proxyTable: {},
 
     // Various Dev Server settings
+    // host: 'www.kunludi.com', // can be overwritten by process.env.HOST
     host: 'localhost', // can be overwritten by process.env.HOST
+    // port: 80, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     port: 8101, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+	
     autoOpenBrowser: false,
     errorOverlay: true,
     notifyOnErrors: true,
